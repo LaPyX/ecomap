8a2f750b03edd4cd27b51ae43c3d3e5081e2e5ac
Maxim Byakow <mbyakow@gmail.com>
2017-01-09 21:03:34 +0600

    #EM-1 Back to articles placement

--- resources/assets/js/components/NewsShow.vue
+++ resources/assets/js/components/NewsShow.vue
@@ -4,7 +4,7 @@
 
         <h1 style="text-align: center; margin: 0 0 1em;">{{ item.name }}</h1>
 
-        <a href="#" @click.prevent="showNews" style="font-size: .9em; margin-bottom: 2em; display: block;">Вернуться к новостям</a>
+        <div style="text-align: center;"><a href="#" @click.prevent="showNews" style="font-size: .9em; margin-bottom: 2em; display: block;">Вернуться к новостям</a></div>
 
         <div class="news" v-html="item.text"></div>
     </div>
