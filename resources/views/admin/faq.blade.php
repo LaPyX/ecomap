9aba0d19c835751ac61aca5d687f66a58c06837d
Maxim Byakow <mbyakow@gmail.com>
2017-01-11 04:27:16 +0600

    #EM-3 Instructions page admin preview

--- resources/views/admin/faq.blade.php
+++ resources/views/admin/faq.blade.php
@@ -8,8 +8,12 @@
     @else
         <form method="post">
             {{ csrf_field() }}
-            <textarea name="faq" class="form-control" cols="20" rows="20">{!! $faq !!}</textarea>
+            <textarea name="faq" class="form-control" cols="20" rows="5">{!! $faq !!}</textarea>
             <input class="btn btn-lg btn-primary" type="submit" value="Сохранить" style="margin-top: 1em;">
         </form>
+
+        <div style="margin-top: 3em;">
+            {!! $faq !!}
+        </div>
     @endif
 @endsection
