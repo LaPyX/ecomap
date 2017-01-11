c21d08ebb220d5d45b093f6d6a8882bf8070d5c6
Maxim Byakow <mbyakow@gmail.com>
2017-01-11 23:28:47 +0600

    #EM-8 Update notification status text fix

--- resources/views/emails/requests/update.blade.php
+++ resources/views/emails/requests/update.blade.php
@@ -1,8 +1,22 @@
+@php
+    switch ($request->status) {
+        case 1:
+            $status = 'На рассмотрении';
+            break;
+        case 2:
+            $status = 'В работе';
+            break;
+        case 3:
+            $status = 'Проблема решена';
+            break;
+    }
+@endphp
+
 <p>Здравствуйте, {{ $request->name }}!</p>
 
 <p>
-    На сайте "Интерактивная карта незаконных свалок" изменился статус обращения #{{ $request->id }}.<br>
-    Новый статус: <strong>{{ $request->status }}</strong>.
+    На сайте "Интерактивная карта незаконных свалок" изменился статус обращения <strong>#{{ $request->id }}</strong>.<br>
+    Новый статус: <strong>{{ $status }}</strong>.
 </p>
 
 @if ($request->comment)
